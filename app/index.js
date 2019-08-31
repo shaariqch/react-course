7beff72dc64a4be4093ec84bafa26aba96bafe57
Shaariq Chundrigar <shaariqch@gmail.com>
2019-08-31 15:21:51 +1000

    Completed part 5 - fetch repos

--- app/index.js
+++ app/index.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Popular from './components/Popular'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Popular from './components/Popular';
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Popular/>
+        <Popular />
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'));
