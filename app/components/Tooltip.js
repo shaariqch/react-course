8502466850b468d037314e585efbc47367c0cf54
Shaariq Chundrigar <shaariqch@gmail.com>
2019-09-04 20:15:19 +1000

    Completed part 21: withHiver higher order component

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import withHover from './withHover';
+import { FaPrescriptionBottle } from 'react-icons/fa';
 const styles = {
   container: {
     position: 'relative',
@@ -23,41 +24,18 @@ const styles = {
   },
 };
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hovering: false,
-    };
-
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-
-  mouseOver() {
-    this.setState({
-      hovering: true,
-    });
-  }
-
-  mouseOut() {
-    this.setState({
-      hovering: false,
-    });
-  }
-
-  render() {
-    const { hovering } = this.state;
-    const { text, children } = this.props;
-    return (
-      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} style={styles.container}>
-        {hovering === true && <div style={styles.tooltip}>{text}</div>}
-        {children}
-      </div>
-    );
-  }
+function Tooltip({ text, children, hovering }) {
+  return (
+    <div style={styles.container}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  );
 }
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
+  hovering: PropTypes.bool.isRequired,
 }
+
+export default withHover(Tooltip, 'hovering');
