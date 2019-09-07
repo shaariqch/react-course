1ed240517840feb30ab93bf75d5f967750008ab0
Shaariq Chundrigar <shaariqch@gmail.com>
2019-09-07 14:10:20 +1000

    Completed part 24: toggle theme

--- app/index.js
+++ app/index.js
@@ -4,27 +4,28 @@ import './index.css';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
 import { ThemeProvider } from './contexts/theme';
-
+import Nav from './components/Nav';
 class App extends React.Component {
-  constructor(prop) {
+  constructor(props) {
     super(props);
 
     this.state = {
       theme: 'light',
       toggleTheme: () => {
-       this.setState(({
-         theme
-       }) => ({
-         theme: theme === 'light' ? 'dark' : 'light'
-       }))
-      }
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light',
+        }));
+      },
     };
   }
   render() {
     return (
       <ThemeProvider value={this.state}>
-        <div className="container">
-          <Battle />
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Battle />
+          </div>
         </div>
       </ThemeProvider>
     );
